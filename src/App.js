20be222ca0d21a8ab1516268871cbacd759a473a
Nischal <nees2076@gmail.com>
2024-08-15 17:52:24 +0545

    cards components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Footer from './Footer'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="main">
+      <Header/>
+      <div className='row'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+      <Footer/>
     </div>
+    
   );
-}
+ }
+ function Card() {
+   return (
 
+     <div className='card'>
+      <div class="details">
+    <div class="cardHeader">CSS Hover Animation</div>
+    <div class="cardText">
+      Welcome to this card where i have made use of the "Hover" effect to add
+      some really cool interactions to it. Enjoy
+    </div>
+    <div class="button">Learn More</div>
+  </div>
+     </div>
+   )
+ }
+ 
 export default App;
